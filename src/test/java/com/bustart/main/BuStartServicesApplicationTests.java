c8d2ce41d0460f289d7a56396bdc56fdfab50de0
DESKTOP-8E4FBA8\Slam245 <eduardoexplod@hotmail.com>
2022-08-30 20:48:06 -0500

    First Version of Application JPA

---
+++ src/test/java/com/bustart/main/BuStartServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bustart.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuStartServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
